547c20eba2e7861c04aae669a705dd16974ced9d
Ahad <ahadhossainkhan729@gmail.com>
2026-07-14 12:42:15 +0600

    Fixed the xyz.c

--- xyz.c
+++ xyz.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+/* If your environment cannot locate system headers, declare printf manually */
+extern int printf(const char *format, ...);
 int main(){
   int a ,b , sum;
   a = 5; b = 5;
   sum = a + b;
   printf("The sum of two numbers is: %d", sum);
-  return 0; 
+  return 0;
+}
