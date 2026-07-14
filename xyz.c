43dca4576f91d3c41f66b4fc98aa42ec08f501d4
Ahad <ahadhossainkhan729@gmail.com>
2026-07-14 13:08:59 +0600

    Deleted some files

--- xyz.c
+++
@@ -1,9 +0,0 @@
-/* If your environment cannot locate system headers, declare printf manually */
-extern int printf(const char *format, ...);
-int main(){
-  int a ,b , sum;
-  a = 5; b = 5;
-  sum = a + b;
-  printf("The sum of two numbers is: %d", sum);
-  return 0;
-}
